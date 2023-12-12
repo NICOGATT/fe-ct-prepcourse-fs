8032320844e9e284e0f030bcb07068082790ccba
NICOGATT <130109204+NICOGATT@users.noreply.github.com>
2023-12-12 11:36:34 -0300

    Terminando el modulo 05

--- M05 Fundamentos Javascript/ejercicios/39.js
+++ M05 Fundamentos Javascript/ejercicios/39.js
@@ -1,8 +1,9 @@
 // ⛔️ Recuerda que debes utilizar el objeto global "Math".
 
-function numeroRandom() {
+function numeroRandom(num) {
   // Genera un número al azar entre 0 y 1 y retórnalo.
   // Tu código:
+  return Math.round(num);
 }
 
 module.exports = numeroRandom;
