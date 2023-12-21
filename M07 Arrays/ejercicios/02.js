dcc6fffb1e518cb5eecfd08bb8f8c79064cf0a57
NICOGATT <130109204+NICOGATT@users.noreply.github.com>
2023-12-21 17:14:57 -0300

    Empezando el modulo 07

--- M07 Arrays/ejercicios/02.js
+++ M07 Arrays/ejercicios/02.js
@@ -2,6 +2,14 @@ function ordenarArray(array) {
   // Ordena los elementos del areglo array de menor a mayor.
   // Devuelve el arreglo resultante.
   // Tu código:
+  const arrayOrdenado = array.sort((a, b) => {
+    if (typeof a === "number" && typeof b === "number") {
+      return a - b;
+    } else {
+      return String(a).localeCompare(String(b))
+    }
+  })
+  return arrayOrdenado; 
 }
 
 module.exports = ordenarArray;
