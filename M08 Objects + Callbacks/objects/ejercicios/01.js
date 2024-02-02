7e396387102059548db9b0a7b2741ea3de83e558
NICOGATT <130109204+NICOGATT@users.noreply.github.com>
2024-02-02 13:11:40 -0300

    Iniciando el M08

--- M08 Objects + Callbacks/objects/ejercicios/01.js
+++ M08 Objects + Callbacks/objects/ejercicios/01.js
@@ -2,6 +2,15 @@ const obtenerValorPropiedad = (objeto, propiedad) => {
   // Recibe un objeto y el nombre de una propiedad.
   // Devuelve el valor de esta propiedad.
   // Tu código:
+  const propiedades = propiedad.split(".");
+  for (let prop of propiedades) {
+    if (objeto && objeto.hasOwnProperty(prop)) {
+      objeto = objeto[prop];
+    } else{
+      return undefined;
+    }
+  }
+  return objeto; 
 };
 
 module.exports = obtenerValorPropiedad;
